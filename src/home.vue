d96a9e6cd8c69454d293f1458eb10c50d67d029d
Ziva <zivalee87@gmail.com>
2020-02-25 00:30:23 +0800

    modify text size

--- src/home.vue
+++ src/home.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <b-container class="mt-5">
-      <h4>Guide:</h4>
-      <p>Choose whether you need us to rate your talent: <router-link to="/analysis">Analysis</router-link></p>
+      <h3>Guide:</h3>
+      <h5 class="mt-3">Choose whether you need us to rate your talent: <router-link to="/analysis">Analysis</router-link></h5>
       
-      <p>Or you need talent recommendation: <router-link to="/recommend">Recommend</router-link></p>
+      <h5 class="mt-2">Or you need talent recommendation: <router-link to="/recommend">Recommend</router-link></h5>
     </b-container>
   </div>
 </template>
